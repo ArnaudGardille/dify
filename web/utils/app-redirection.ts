dea302348d63c58785d8e011c5e5ceb0292a3e4b
“Arnaud“ <arnaud.gardille@gmail.com>
2024-07-18 08:42:41 +0000

    corrected app url

--- web/utils/app-redirection.ts
+++ web/utils/app-redirection.ts
@@ -1,10 +1,14 @@
+import { useStore as useAppStore } from '@/app/components/app/store'
+
 export const getRedirection = (
   isCurrentWorkspaceEditor: boolean,
   app: any,
   redirectionFunc: (href: string) => void,
 ) => {
   if (!isCurrentWorkspaceEditor) {
-    redirectionFunc(`/app/${app.id}/overview`)
+    const appURL = `${app.mode}/${app.site.app_base_url}/${app.site.access_token}`
+
+    redirectionFunc(appURL)
   }
   else {
     if (app.mode === 'workflow' || app.mode === 'advanced-chat')
