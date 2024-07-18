fea64ef978e7db9a65f1654e6fc7b3282fb62f2e
ArnaudGardille <arnaud.gardille@gmail.com>
2024-07-18 18:32:13 +0200

    back to working stuff

--- web/utils/app-redirection.ts
+++ web/utils/app-redirection.ts
@@ -1,26 +1,10 @@
-import { useStore as useAppStore } from '@/app/components/app/store'
-
 export const getRedirection = (
   isCurrentWorkspaceEditor: boolean,
   app: any,
   redirectionFunc: (href: string) => void,
 ) => {
   if (!isCurrentWorkspaceEditor) {
-    const appBaseUrl = app.site?.app_base_url || "";
-    const accessToken = app.site?.access_token || "";
-    if (!appBaseUrl) {
-      console.error("Error: Missing app base URL");
-      // Optionally, redirect to an error page or display a message
-      return;
-    }
-    if (!accessToken) {
-      console.error("Error: Missing access token");
-      // Optionally, redirect to an error page or display a message
-      return;
-    }
-    
-    const appURL = `${app.mode}/${appBaseUrl}/${accessToken}`;
-    redirectionFunc(appURL);
+    redirectionFunc(`/app/${app.id}/overview`)
   }
   else {
     if (app.mode === 'workflow' || app.mode === 'advanced-chat')
